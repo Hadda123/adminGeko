f02128eeb63da8d3832c2ff9ecf194df9039fd97
unknown <imed.siala@gmail.com>
2023-11-23 10:24:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {BrowserRouter as Router , Routes , Route} from "react-router-dom"
+import Login from './pages/Login';
+import Resetpassword from './pages/Resetpassword';
+import Forgetpassword from './pages/Forgetpassword';
+import MainLayout from './components/MainLayout';
+import Dashboard from './pages/Dashboard';
+import Enquiries from './pages/Enquiries';
+import Bloglist from './pages/Bloglist';
+import Blogcatlist from './pages/Blogcatlist';
+import Orders from './pages/Orders';
+import Customers from './pages/Customers';
+import Colorlist from './pages/Colorlist';
+import Categorylist from './pages/Categorylist';
+import Brandlist from './pages/Brandlist';
+import Productlist from './pages/Productlist';
+import Addblog from './pages/Addblog';
+import Addblogcat from './pages/Addblogcat';
+import AddColor from './pages/AddColor';
+import Addcat from './pages/Addcat';
+import Addbrand from './pages/Addbrand';
+import Addproduct from './pages/Addproduct';
+import Couponlist from './pages/couponlist';
+import AddCoupon from './pages/AddCoupon';
+import ViewEnq from './pages/viewEnq';
+import ViewOrder from './pages/ViewOrder';
+import Productlistpendding from './pages/Productlistpending';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+<Routes>
+  <Route path='/' element={<Login/>} />
+  <Route path='/reset-password' element={<Resetpassword />} />
+  <Route path='/forget-password' element={<Forgetpassword />} />
+  <Route path='/admin' element={<MainLayout />} >
+    <Route  index element={<Dashboard/>} />
+    <Route path='enquiries' element={<Enquiries/>} />
+    <Route path='blog' element={<Addblog/>} />
+    <Route path='blog/:id' element={<Addblog/>} />
+    <Route path='blog-list' element={<Bloglist/>} />
+    <Route path='blog-category-list' element={<Blogcatlist/>} />
+    <Route path='orders' element={<Orders/>} />
+    <Route path='order/:id' element={<ViewOrder/>} />
+    <Route path='customers' element={<Customers/>} />
+    <Route path='list-color' element={<Colorlist/>} />
+    <Route path='color' element={<AddColor/>} />
+    <Route path='color/:id' element={<AddColor/>} />
+    <Route path='list-category' element={<Categorylist/>} />
+    <Route path='category' element={<Addcat/>} />
+    <Route path='category/:id' element={<Addcat/>} />
+    <Route path='list-brand' element={<Brandlist/>} />
+    <Route path='brand' element={<Addbrand/>} />
+    <Route path='brand/:id' element={<Addbrand/>} />
+        <Route path='product-list' element={<Productlist/>} />
+        <Route path='product-list' element={<Productlist/>} />
+        <Route path='product-list-pending' element={<Productlistpendding/>} />
+        <Route path='blog-category' element={<Addblogcat/>} />
+        <Route path='blog-category/:id' element={<Addblogcat/>} />
+        <Route path='product' element={<Addproduct/>} />
+        <Route path='product/:codart/:desart/:stkfin/:PURNTTC' element={<Addproduct/>} />
+        <Route path="coupon-list" element={<Couponlist />} />
+          <Route path="coupon" element={<AddCoupon />} />
+          <Route path="coupon/:id" element={<AddCoupon />} />
+
+          <Route path='enquiries/:id' element={<ViewEnq/>} />
+  </Route>
+
+</Routes>
+    </Router>
   );
 }
 
---
+++ src/state/api.js
@@ -0,0 +1,4 @@
+import axios from 'axios';
+export const getArticles= async () => {
+    return await axios.get("https://www.24-crm.com/webservice/api/ARTICLEs?coddos=comaf&serveur=/]qpmz&user=tb&pwd=Qpmztpgubinfenf{hboj234&database=PSCOMAF");
+  }
